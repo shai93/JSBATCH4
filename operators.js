f5630e3b7fb5a095774a5eede35de7dc3e111d8a
shailesh.salekar <shailesh.salekar@accionlabs.com>
2020-05-09 21:45:24 +0530

    if, else, for , while, do while, break , continue

---
+++ operators.js
@@ -0,0 +1,168 @@
+// logical, comparision
+
+// logical  (and, or, not)
+
+// let x = 25;
+// let y = 25;
+// let z = true;
+
+// comparision operators (==, ===, !=)
+
+
+//   marks is equal to 45 // just passed
+//  greater than 45 and less than 60  // second class
+// greter than 60 and less than 75  //first class
+// greater than 75 //distinction
+//less than 45 // failed
+
+// let marks = 25;
+
+// if(marks > 45 && marks < 60){
+//     console.log("Second Class")
+// }
+// else if(marks >= 60 && marks <75){
+//     console.log("first CLass")
+// }else if(marks >= 75){
+//     console.log("distinction")
+// }
+// else if( marks < 45){
+//     console.log("Failed")
+// }else if(marks == 45){
+//     console.log("Passed")
+// }
+
+
+// Bitwise Opertor
+
+// 1 byte  = 8 bit (1,0)
+// 64 bit 
+// 1,0 = 2
+
+
+// 
+
+
+// 2^0 = 1
+// 2^1 = 2
+// 2^2 = 4
+// 2^3 = 8
+
+
+// XOR and Not in bitwise
+
+
+// let s = 5;          // 0101
+// let d = 1;          // 0001
+//                     // 0101 =  5
+// console.log(s|d)
+
+// Switch Statements == nested if else
+
+// let x =  new Date().getDay();
+// let x = "1";
+
+// // ===
+// switch(x){
+//     case 1:
+//     case "1":
+//         console.log("Monday")
+//         break;
+//     case 2:
+//         console.log("Tuesday")
+//         break;
+//     case 3:
+//         console.log("Wednesday")
+//         break;
+//     case 4:
+//         console.log("Thursday")
+//         break;
+//     case 5:
+//         console.log("Friday")
+//         break;
+//     case 6:
+//         console.log("Saturday")
+//         break;
+//     case 7:
+//         console.log("Sunday")
+//         break;
+//     default:
+//         console.log("Not found")
+// }
+
+
+
+let y  = "helloworld";
+
+// for(let i =0 ; i< y.length; i++){
+//         console.log(y[i])
+// }
+let i = 0;
+while(i < y.length){            //1 <10
+    if(i  == 1){                //1 == 1
+        i++;
+        continue;                // continue
+    }
+    console.log(y[i]);
+    i++;            
+}
+// Steps
+
+// i = 0;  h
+//  i = 1;
+// i = 2  l
+
+
+
+
+
+// let k = 1
+// do{
+//     console.log('hello');
+// }
+// while(k>1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if(x === y){
+//     console.log(' x  is equal to  y')
+// }
+
+
+
+// if(x>=25 && (y>x || x>y)){
+//     console.log(x)
+// }
+
+// true && false = false
+// true && true = true
+
+// true || false = true
+// false ||  true = true
+// true || true = true
+
+
+// !true = false
+// !false = true
+
+// if(!(x > 10)){
+//     console.log('Condition satisfied!!!!');
+// }
+
+
+
+
