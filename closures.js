801cc87257bf16094a0f6536986b11dfe3f58761
shailesh.salekar <shailesh.salekar@accionlabs.com>
2020-06-28 16:26:56 +0530

    fetch code

--- closures.js
+++ closures.js
@@ -27,3 +27,14 @@ result(50)
 result(60)
 result(70)
 
+
+
+async function getTodos(){
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1'); //promise1
+    const json = await response.json();  //promise2
+    console.log(json)
+}
+
+
+
+getTodos()
